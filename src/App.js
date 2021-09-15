d09e5f198f949010bcd32c519e54fff50e366a22
Danil Savchenko <danil.savchenko.dev@gmail.com>
2021-09-15 14:31:02 +0600

    Finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import { useEffect, useState } from "react";
+
+function App({ text }) {
+  const words = text.split(" ");
+
+  const [cursor, setCursor] = useState(0);
+  const [value, setValue] = useState("");
+  const [timerId, setTimerId] = useState(null);
+  const [timer, setTimer] = useState(0);
+  const [speed, setSpeed] = useState(0);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+
+    if (cursor === 0 && timerId === null) {
+      setTimer(0);
+      setTimerId(
+        window.setInterval(() => {
+          setTimer((t) => t + 1);
+        }, 1000)
+      );
+    }
+  };
+
+  useEffect(() => {
+    if (value[value.length - 1] === " ") {
+      if (value.trim() === words[cursor]) {
+        setCursor(cursor + 1);
+
+        if (cursor === words.length - 1) {
+          setCursor(0);
+          setTimerId(null);
+          window.clearInterval(timerId);
+        }
+      }
+
+      setValue("");
+    }
+  }, [value, cursor]);
+
+  useEffect(() => {
+    setSpeed(words.slice(0, cursor).join(" ").length / timer || 0);
+  }, [timer]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        {words.map((t, idx) => (
+          <span key={idx}>
+            <i
+              style={{
+                background: cursor === idx ? "#ccc" : "none",
+                color:
+                  cursor === idx && value && t.indexOf(value.trim()) === -1
+                    ? "red"
+                    : "#000",
+              }}
+            >
+              {t}
+            </i>{" "}
+          </span>
+        ))}
+      </p>
+
+      <input onChange={handleChange} value={value} />
+
+      <p>
+        Потраченное время: <b>{timer} сек</b>
+      </p>
+      <p>
+        Скорость печати: <b>{(speed * 60).toFixed(2)} символов в мин</b>
+      </p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
+
+const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris dapibus ante id elementum feugiat. Proin id velit elit. Sed ac diam ullamcorper, tincidunt mauris ut, fringilla eros. Maecenas lectus diam, vulputate ut mauris non, lobortis varius eros. Duis vulputate, metus eu imperdiet blandit, nunc diam aliquam nisl, id rutrum ex libero quis neque. Maecenas imperdiet eget arcu ac finibus. Fusce eget massa metus. Pellentesque quis vulputate velit, id aliquam est. Donec accumsan massa id risus iaculis, at semper massa cursus. Etiam mattis, justo sit amet accumsan interdum, odio magna consectetur elit, vitae finibus ligula nisi ac sem. Vestibulum sit amet porttitor elit.`;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App text={text} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
